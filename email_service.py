6de41f829f62c089996fdcdad2605b73ecb546df
codebynishan <mail.nishanrana@gmail.com>
2025-11-08 13:29:13 +0545

    Email sending Functionality

---
+++ email_service.py
@@ -0,0 +1,26 @@
+import os
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from dotenv import load_dotenv
+
+load_dotenv()
+EMAIL_USER = os.getenv("EMAIL_USER")
+EMAIL_PASS = os.getenv("EMAIL_PASS")
+
+def send_email(subject, body, to_email):
+   
+    msg = MIMEMultipart()
+    msg["From"] = EMAIL_USER
+    msg["To"] = to_email
+    msg["Subject"] = subject
+    msg.attach(MIMEText(body, "plain"))
+
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as server:
+            server.starttls()
+            server.login(EMAIL_USER, EMAIL_PASS)
+            server.send_message(msg)
+            print("Email sent successfully!")
+    except Exception as e:
+        print("Failed to send email:", e)
